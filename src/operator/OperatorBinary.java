348aa5339cfe09ae6adbb55b111cb93da7d97d0b
faudha <syfudhfaudhansm@gmail.com>
2021-04-20 15:45:22 +0800

    Operator Binary Aritmatika

--- src/operator/OperatorBinary.java
+++ src/operator/OperatorBinary.java
@@ -4,4 +4,13 @@ public class OperatorBinary {
     String nama = "Faudha Nur Sidiq Mudyyariansyah";
     int jumlahSks = 24;
     double indeksPrestasiKumulatif = 3.52;
+
+    public static void main(String[] args) {
+        System.out.println(9+3);
+        System.out.println(9-3);
+        System.out.println(9*3);
+        System.out.println(9/3);
+        System.out.println(9%3);
+        System.out.println(9%2);
+    }
 }
