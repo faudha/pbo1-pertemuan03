d3010932506438531b57c25817587b3f66a4ae7d
faudha <syfudhfaudhansm@gmail.com>
2021-04-20 23:25:14 +0800

    Ubah total belanja = uang di dompet. uang pas

--- src/percabangan/IfElse3.java
+++ src/percabangan/IfElse3.java
@@ -2,7 +2,7 @@ package percabangan;
 
 public class IfElse3 {
     public static void main(String[] args) {
-        double totalBelanja = 21000;
+        double totalBelanja = 25000;
         double uangDidompet = 25000;
 
         if(uangDidompet<totalBelanja){
